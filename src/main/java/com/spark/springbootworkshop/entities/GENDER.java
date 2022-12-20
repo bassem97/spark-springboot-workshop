7d899d19c579d48f38e2ea4d05cfc62628755481
BASSEM <bassem.jadoui@esprit.tn>
2022-12-20 09:48:59 +0100

    student entity done ✅

---
+++ src/main/java/com/spark/springbootworkshop/entities/GENDER.java
@@ -0,0 +1,6 @@
+package com.spark.springbootworkshop.entities;
+
+public enum GENDER {
+    MALE,
+    FEMALE
+}
---
+++ src/main/java/com/spark/springbootworkshop/entities/Student.java
@@ -0,0 +1,28 @@
+package com.spark.springbootworkshop.entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity @Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+@ToString
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idStudent;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    private GENDER gender;
+
+
+
+
+}
