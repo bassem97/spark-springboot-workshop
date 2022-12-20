a770e34887a8eb0d7757830077ac698ea7cb4548
BASSEM <bassem.jadoui@esprit.tn>
2022-12-20 09:33:30 +0100

    Initial commit

---
+++ src/test/java/com/spark/springbootworkshop/SpringbootWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spark.springbootworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWorkshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
